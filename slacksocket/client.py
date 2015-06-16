02661220279e95ea922971d8cb3777334ade0c22
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-06-16 10:23:01 -0400

    add slackclient requests session subclass, update methods to use

--- slacksocket/client.py
+++ slacksocket/client.py
@@ -33,6 +33,52 @@ class SlackEvent(object):
         self.json = json.dumps(event)
         self.event = event
 
+class SlackClient(requests.Session):
+    """
+    """
+    def __init__(self, token):
+        super(Client, self).__init__()
+        self.token = token
+
+    def get_json(self,url):
+        return self._result(self._get(url))
+
+    def _post(self, url):
+        return self.post(url, params={'token':self.token})
+
+    def _get(self, url):
+        return self.get(url, params={'token':self.token})
+
+    def _result(self, res):
+        try:
+            res.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            raise errors.SlackSocketAPIError(e, res, explanation=explanation)
+
+        rj = res.json
+        if not rj['ok']:
+            raise SlackSocketAPIError('Error from slack api:\n %s' % r.text)
+
+        return rj
+
+class SlackMsg(object):
+    """
+    Slack default formatted message capable of being sent via the RTM API
+    params:
+     - text(str)
+     - channel(str)
+    attributes:
+     - type: Slack event type
+     - time: UTC time event was received 
+    """
+    def __init__(self,id,text,channel):
+        self.msgdict = { 'id'      : id,
+                         'text'    : text,
+                         'channel' : channel,
+        self.time = int(time.time())
+        self.json = json.dumps(event)
+        self.event = event
+
 class SlackSocket(object):
     """
     SlackSocket class provides a streaming interface to the Slack Real Time
@@ -46,18 +92,11 @@ class SlackSocket(object):
         if type(translate) != bool:
             raise TypeError('translate must be a boolean')
         self.events = []
-        self.token = slacktoken
         self.translate = translate
-        self.team,self.user = self._auth_test()
-        self.thread = thread.start_new_thread(self.open,())
+        self.client = SlackClient(slacktoken)
 
-    def open(self):
-        ws = websocket.WebSocketApp(self._get_websocket_url(),
-                                    on_message = self._event_handler,
-                                    on_error   = self._error_handler,
-                                    on_open    = self._open_handler,
-                                    on_close   = self._exit_handler)
-        ws.run_forever()
+        self.team,self.user = self._auth_test()
+        self.thread = thread.start_new_thread(self._open,())
 
     def get_event(self,event_filter='all'):
         """
@@ -89,36 +128,43 @@ class SlackSocket(object):
             except IndexError:
                 pass
 
+    def send_msg(self,text):
+        """
+        send a message via Slack RTM socket
+        """
+        pass
+        
     #######
     # Internal Methods
     #######
 
+    def _open(self):
+        #reset id for sending messages with each new socket
+        self.send_id = 1
+        ws = websocket.WebSocketApp(self._get_websocket_url(),
+                                    on_message = self._event_handler,
+                                    on_error   = self._error_handler,
+                                    on_open    = self._open_handler,
+                                    on_close   = self._exit_handler)
+        ws.run_forever()
+
     def _get_websocket_url(self):
         """
-        retrieve a fresh websocket url from slack api
+        Retrieve a fresh websocket url from slack api
         """
-        r = requests.get(slackurl['rtm'],params={'token':self.token})
-        rj = r.json()
-        if not rj['ok']:
-            raise SlackSocketAPIError('Error from slack api:\n %s' % r.text)
-
-        return rj['url']
+        return self.client.get_json(slackurl['rtm'])['url']
 
     def _auth_test(self):
         """
         Perform API auth test and get our user and team
         """
-        r = requests.get(slackurl['test'],params={'token':self.token})
-        rj = r.json()
-        if not rj['ok']:
-            raise SlackSocketAPIError('Error from slack api:\n %s' % r.text)
+        test = self.client.get_json(slackurl['test'])
         
         if self.translate:
-            return (rj['team'],rj['user'])
+            return (test['team'],test['user'])
         else:
-            return (rj['team_id'],rj['user_id'])
+            return (test['team_id'],test['user_id'])
 
-    #TODO: add ability for lookup via id and cname
     def _lookup_user(self,user_id):
         """
         Look up a username from user id
@@ -126,12 +172,9 @@ class SlackSocket(object):
         if user_id == 'USLACKBOT':
             return "slackbot"
 
-        r = requests.get(slackurl['users'],params={'token':self.token})
-        rj = r.json()
-        if not rj['ok']:
-            log.critical('error from slack api:\n %s' % r)
+        members = self.client.get_json(slackurl['users'])
 
-        for user in rj['members']:
+        for user in members:
             if user['id'] == user_id:
                 return user['name']
         else:
@@ -143,11 +186,8 @@ class SlackSocket(object):
         Look up a channelname from channel id
         """
         for channel_type in ['channels','groups','ims']:
-            r = requests.get(slackurl[channel_type],params={'token':self.token})
-            rj = r.json()
-            if not rj['ok']:
-                log.critical('error from slack api:\n %s' % r)
-            for channel in rj[channel_type]:
+            clist = self.client.get_json(slackurl[channel_type])[channel_type]
+            for channel in clist:
                 if channel['id'] == channel_id:
                     if channel_type == 'ims':
                         return { 'channel_type' : channel_type,
@@ -160,14 +200,14 @@ class SlackSocket(object):
                  'channel_name' : 'unknown' }
 
     #######
-    # Handlers
+    # Websocket Handlers
     #######
 
     def _event_handler(self,ws,event):
         log.debug('event recieved: %s' % event)
         event = json.loads(event)
 
-        #TODO: make use of channel_type returned from _lookup_channel
+        #TODO: make use of ctype returned from _lookup_channel
         if self.translate:
             if event.has_key('user'):
                 event['user'] = self._lookup_user(event['user'])
